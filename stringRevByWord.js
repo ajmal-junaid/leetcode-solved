90c3f109878d3db3c0210e7dbdcca9110f299716
Ajmal junaid <ajmal6201@gmail.com>
2023-05-15 16:54:17 +0530

    15 may

---
+++ stringRevByWord.js
@@ -0,0 +1,18 @@
+function reverseWords(sentence) {
+    const words = sentence.split(' ');
+  
+    const reversedWords = words.map((word) => {
+      let reversedWord = '';
+  
+      for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word[i];
+      }
+  
+      return reversedWord;
+    });
+  
+    return reversedWords.join(' ');
+  }
+  
+  console.log(reverseWords('Hello world')); // "olleH dlrow"
+  
